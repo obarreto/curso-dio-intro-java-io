cf63242dbcd9998f1164a4f4b702b54d676f578d
Ozeias De Santana Barreto <ozbarreto@outlook.com>
2022-01-20 21:42:14 -0300

    first commit

--- src/br/com/dio/java/io/IOCharacter/ExemploIOBytes.java
+++ src/br/com/dio/java/io/IOCharacter/ExemploIOBytes.java
@@ -9,10 +9,28 @@ public class ExemploIOBytes {
 
 		InputStream is = new FileInputStream(f.getName());
 		BufferedInputStream bis = new BufferedInputStream(is);
+		
+		String nomeArquivoCopia = nomeArquivo.substring(0, nomeArquivo.indexOf("-")).concat(".copy2.txt");
+		File arqCopia = new File(nomeArquivoCopia);
+		
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(nomeArquivoCopia));
+		
+		int line = 0;
+		while((line = bis.read()) != -1) {
+			bos.write((char) line);
+			bos.flush();
+		}
+		
+		bis.close();
+		bos.close();
+		
+		PrintStream ps = new PrintStream(System.out);
+		ps.printf("Arquivo \"%s\" copiado com sucesso! Tamanho do arquivo ' %d' bytes. No diretorio ' %s'\n", f.getName(), f.length(), f.getPath());
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		copiarArquivo();
 
 	}
 
--- src/br/com/dio/java/io/IOCharacter/Exercicio1IOCaracter.java
+++ src/br/com/dio/java/io/IOCharacter/Exercicio1IOCaracter.java
@@ -1,12 +1,6 @@
 package br.com.dio.java.io.IOCharacter;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Exercicio1IOCaracter {
 
